7b0af1a25d2730a0aa5673d026580843a188dd2d
Talya Ohana <twhnh02633@gmail.com>
2024-11-15 22:59:39 +0200

    added Vector function in Vector cpp

--- Vector.cpp
+++ Vector.cpp
@@ -1,2 +1,14 @@
 #include "Vector.h"
 
+Vector::Vector(int n)
+{
+	if (n < 2)
+		n == 2;
+	
+	_size = 0;
+	_capacity = n;
+	_resizeFactor = n;
+
+	this->_elements = new int[this->_resizeFactor];
+}
+ 
